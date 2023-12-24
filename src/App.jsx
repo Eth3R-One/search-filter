734007e1cd0580599153558a822927415e2b6377
Eth3R-One <refaythii@gmail.com>
2023-12-24 16:02:13 +0600

    implemented search for firstname, lastname and email

--- src/App.jsx
+++ src/App.jsx
@@ -5,28 +5,31 @@ import { Table } from "./Table";
 
 function App() {
   const [search, setSearch] = useState("");
-  const handleSearch = (e) => {
-    setSearch(e.target.value);
-    console.log(search);
-  };
+  const [searchCount, setSearchCount] = useState(0);
+
   return (
     <>
-      <h1 className="text-3xl font-bold">Search Filter</h1>
-      <div className="container mx-auto px-4 md:container md:mx-auto">
-        <div className="flex flex-">
+      <h1 className="text-3xl font-bold align-content-center">Search Filter</h1>
+      <div className="container mx-auto px-4 align-content-center md:container md:mx-auto">
+        <div className="flex mx-auto flex-col p-4 m-4 align-center">
           <input
-            onChange={handleSearch}
-            className="s-xl"
+            className="s-xl p-3"
             type="text"
             placeholder="Search..."
+            onChange={(e) => {
+              setSearch(e.target.value.toLowerCase());
+            }}
           />
-          <button onClick={handleSearch} className="m-3">
-            Search
-          </button>
+          <button className="m-3 s-xl">Search</button>
+          {(search !== "") & <div>Count:`${searchCount}`</div>}
         </div>
       </div>
-      <div className="flex m-auto p-10">
-        <Table />
+      <div className="flex m-auto p-5 md:container md:mx-auto">
+        <Table
+          search={search}
+          searchCount={searchCount}
+          setSearchCount={setSearchCount}
+        />
       </div>
     </>
   );
--- src/Table.jsx
+++ src/Table.jsx
@@ -1,18 +1,30 @@
 import { Card, Typography } from "@material-tailwind/react";
 import TABLE_ROWS from "./MOCK_DATA.json";
+import { useState } from "react";
 
-const TABLE_HEAD = ["", "First Name", "Last Name", "Email", "Gender"];
+const TABLE_HEAD = ["", "Name", "Email", "Gender"];
 
-export function Table() {
+export function Table({ search, searchCount = 0, setSearchCount }) {
+  const isSearchedValid = (person) => {
+    if (
+      person.first_name.toLowerCase().includes(search) ||
+      person.last_name.toLowerCase().includes(search) ||
+      person.email.toLowerCase().includes(search) ||
+      person.gender.toLowerCase().includes(search)
+    ) {
+      return true;
+    }
+    return false;
+  };
   return (
-    <Card className="h-full w-full overflow-auto ">
+    <Card className="h-full w-full ">
       <table className="w-full min-w-max table-auto text-center">
         <thead>
           <tr>
             {TABLE_HEAD.map((head) => (
               <th
                 key={head}
-                className="border-b border-blue-gray-100 bg-blue-gray-50 p-4 md:h-auto md:w-auto"
+                className="border-b border-blue-gray-100 bg-blue-gray-50 p-4"
               >
                 <Typography
                   variant="small"
@@ -26,66 +38,71 @@ export function Table() {
           </tr>
         </thead>
         <tbody>
-          {TABLE_ROWS.map(
-            ({ id, first_name, last_name, email, gender }, index) => {
-              const isLast = index === TABLE_ROWS.length - 1;
-              const classes = isLast
-                ? "p-4"
-                : "p-4 border-b border-blue-gray-50";
-
-              return (
-                <tr key={id}>
-                  <td className={classes}>
-                    <Typography
-                      as="a"
-                      href="#"
-                      variant="small"
-                      color="blue-gray"
-                      className="font-medium"
-                    >
-                      {id}
-                    </Typography>
-                  </td>
-                  <td className={classes}>
-                    <Typography
-                      variant="small"
-                      color="blue-gray"
-                      className="font-normal"
-                    >
-                      {first_name}
-                    </Typography>
-                  </td>
-                  <td className={classes}>
-                    <Typography
-                      variant="small"
-                      color="blue-gray"
-                      className="font-normal"
-                    >
-                      {last_name}
-                    </Typography>
-                  </td>
-                  <td className={classes}>
-                    <Typography
-                      variant="small"
-                      color="blue-gray"
-                      className="font-normal"
-                    >
-                      {email}
-                    </Typography>
-                  </td>
-                  <td className={classes}>
-                    <Typography
-                      variant="small"
-                      color="blue-gray"
-                      className="font-medium"
-                    >
-                      {gender === "M" ? "Male" : "Female"}
-                    </Typography>
-                  </td>
-                </tr>
-              );
+          {TABLE_ROWS.filter((person) => {
+            if (search === "") return person;
+            // else if (person.first_name.toLowerCase().includes(search))
+            //   return person;
+            else if (
+              person.first_name.toLowerCase().includes(search) ||
+              person.last_name.toLowerCase().includes(search) ||
+              person.email.toLowerCase().includes(search)
+            ) {
+              // setSearchCount((prev) => prev + 1);
+              // setSearchCount((prev) => prev + 1);
+              return person;
             }
-          )}
+            // isSearchedValid(person);
+          }).map(({ id, first_name, last_name, email, gender }, index) => {
+            const isLast = index === TABLE_ROWS.length - 1;
+            const classes = isLast
+              ? "p-4"
+              : "p-4 border-b  border-blue-gray-50";
+            // setSearchCount(countSearcch);
+            console.log(searchCount);
+            return (
+              <tr key={id}>
+                <td className={classes}>
+                  <Typography
+                    as="a"
+                    href="#"
+                    variant="small"
+                    color="blue-gray"
+                    className="font-medium"
+                  >
+                    {id}
+                  </Typography>
+                </td>
+                <td className={classes}>
+                  <Typography
+                    variant="small"
+                    color="blue-gray"
+                    className="font-normal"
+                  >
+                    {first_name} {last_name}
+                  </Typography>
+                </td>
+
+                <td className={classes}>
+                  <Typography
+                    variant="small"
+                    color="blue-gray"
+                    className="font-normal"
+                  >
+                    {email}
+                  </Typography>
+                </td>
+                <td className={classes}>
+                  <Typography
+                    variant="small"
+                    color="blue-gray"
+                    className="font-medium"
+                  >
+                    {gender === "M" ? "Male" : "Female"}
+                  </Typography>
+                </td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
     </Card>
